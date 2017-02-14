36fd56e1d909a72f11ac15a044ff26f2c43e2c42
XICHEN24 <xichen24@illinois.edu>
2017-02-14 11:43:30 -0600

    change input format

--- src/clustering_eval_toolbox.py
+++ src/clustering_eval_toolbox.py
@@ -14,10 +14,23 @@ class ColumnType(Enum):
     CONTINUOUS = "continuous"
     CATEGORICAL = "categorical"
 
+def combine_phenotype_data_and_clustering(run_parameters):
+    """This is to insert the sample clusters column into the phenotype dataframe.
 
-def run_post_processing_phenotype_clustering_data(run_parameters):
+    Returns:
+        phenotype_df: phenotype dataframe with the first column as sample clusters.
+    """
+    phenotype_df = pd.read_csv(run_parameters['phenotype_data_full_path'], index_col=0, header=0, sep='\t')
+    phenotype_df.insert(0, 'Cluster_ID', np.nan)
+    cluster_labels_df = pd.read_csv(run_parameters['cluster_mapping_full_path'], index_col=0, header=None, sep='\t')
+    phenotype_df.loc[cluster_labels_df.index.values, 'Cluster_ID'] = cluster_labels_df.values
+    return phenotype_df
+
+def run_post_processing_phenotype_clustering_data(cluster_phenotype_df, run_parameters):
     """This is the clean up function of phenotype data with nans removed.
 
+    Parameters:
+        phenotype_df: phenotype dataframe with the first column as sample clusters.
     Returns:
         output_dict: dictionary with keys to be categories of phenotype data and values
         to be a list of related dataframes.
@@ -25,7 +38,7 @@ def run_post_processing_phenotype_clustering_data(run_parameters):
     from knpackage.toolbox import get_spreadsheet_df
     from collections import defaultdict
 
-    cluster_phenotype_df = get_spreadsheet_df(run_parameters['phenotype_data_full_path'])
+    # cluster_phenotype_df = get_spreadsheet_df(run_parameters['phenotype_data_full_path'])
     output_dict = defaultdict(list)
 
     for column in cluster_phenotype_df:
@@ -62,7 +75,7 @@ def f_oneway(phenotype_df):
 
     fval, pval = stats.f_oneway(*groups)
     uniq_trait = np.unique(phenotype_df.values[:, 1].reshape(-1))
-    ret = [len(uniq_trait), 'f_oneway', fval, pval]
+    ret = ['f_oneway', len(uniq_trait), phenotype_df.shape[0], fval, pval]
     return ret
 
 def chisquare(phenotype_df):
@@ -75,6 +88,7 @@ def chisquare(phenotype_df):
 
     unique_category = np.unique(phenotype_df.values[:, 1])
     unique_cluster = np.unique(phenotype_df.values[:, 0])
+
     num_clusters = len(unique_cluster)
     num_phenotype = len(unique_category)
     phenotype_name = phenotype_df.columns.values[1]
@@ -97,18 +111,21 @@ def chisquare(phenotype_df):
     cluster_total_pct = 1.0 * cluster_total / total_total
     phenotype_total_pct = 1.0 * phenotype_total / total_total
     expect = (np.array([cluster_total_pct]).T * phenotype_total)  # pylint: disable=no-member
-    dd = (cont_table.shape[0] - 1) * (cont_table.shape[1] - 1)
+    dd = cont_table.shape[0] + cont_table.shape[1] - 2
     chi, pval = stats.chisquare(cont_table, expect, ddof=dd, axis=None)
-    ret = [num_phenotype, 'chisquare', chi, pval]
-    return ret
+    # chi, pval, dof, expected = stats.chi2_contingency(cont_table)
 
+    ret = ['chisquare', num_phenotype, phenotype_df.shape[0], chi, pval]
+    return ret
 
 def clustering_evaluation(run_parameters):
     """ Run clustering evaluation on the whole dataframe of phenotype data.
     Save the results to tsv file.
     """
-    output_dict = run_post_processing_phenotype_clustering_data(run_parameters)
-    result_df = pd.DataFrame(index=['Trait_length_after_dropna', 'measure', 'chi/fval', 'pval'])
+    cluster_phenotype_df = combine_phenotype_data_and_clustering(run_parameters)
+    output_dict = run_post_processing_phenotype_clustering_data(cluster_phenotype_df, run_parameters)
+    result_df = pd.DataFrame(
+        index=['Measure', 'Trait_length_after_dropna', 'Sample_number_after_dropna', 'chi/fval', 'pval'])
 
     for key, df_list in output_dict.items():
         if(key == ColumnType.CATEGORICAL):
@@ -119,7 +136,6 @@ def clustering_evaluation(run_parameters):
             for item in df_list:
                 phenotype_name = item.columns.values[1]
                 result_df[phenotype_name] = f_oneway(item)
-
 
     file_name = kn.create_timestamped_filename("clustering_evaluation_result", "tsv")
     file_path = os.path.join(run_parameters["results_directory"], file_name)
